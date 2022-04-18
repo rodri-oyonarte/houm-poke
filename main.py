888ae20ed23e4d5f634e0dbf52d604ca1a924c39
rodri-oyonarte <oyonarte.90@gmail.com>
2022-04-18 18:10:56 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# funcion para obtener cuantos pokemon tiene at en su nombre y ademas tiene dos a. Retona un numero
+def get_pokemon_at():
+    pass
+
+
+# funcion para conocer con cuantas especies de pokemon puede procrear raichu. Retorna un numero
+def raichu_egg():
+    pass
+
+
+# funcion para conocer el peso maximo y minimo de los pokemon de tipo fighting de primera generacion.
+# retorna lista [MAX,min]
+def cotas_peso_fighting():
+    pass
