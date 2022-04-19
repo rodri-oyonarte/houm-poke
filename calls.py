a8dbb03db816316e5340cdb8471ef515d9bee1aa
rodri-oyonarte <oyonarte.90@gmail.com>
2022-04-19 16:55:37 -0300

    raichu

--- calls.py
+++ calls.py
@@ -17,12 +17,23 @@ def get_all_pokemon():
 
 # funcion para obtener los egg group de una especie
 def get_egg_group_especie(especie):
-    return 0
+    request = requests.get("https://pokeapi.co/api/v2/pokemon-species/{}/".format(especie))
+    text = json.loads(request.text)
+    egg_group = text['egg_groups']
+
+    return egg_group
 
 
 # funcion para obtener los pokemon de un mismo egg group
-def get_pokemon_por_egg_group():
-    return 0
+def get_pokemon_por_egg_group(egg_group):
+    request = requests.get(egg_group['url'])
+    text = json.loads(request.text)
+    pokemones_egg = []
+
+    for pokemon in text['pokemon_species']:
+        pokemones_egg.append(pokemon['name'])
+
+    return pokemones_egg
 
 
 # funcion para obtener pokemones por tipo
@@ -34,3 +45,6 @@ def get_pokemon_por_tipo():
 def get_pokemon(name):
     pokemon = 0
     return pokemon
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from calls import get_all_pokemon
+from calls import get_all_pokemon, get_egg_group_especie, get_pokemon_por_egg_group
 
 
 # funcion para obtener cuantos pokemon tiene at en su nombre y ademas tiene dos a. Retona un numero
@@ -10,21 +10,35 @@ def get_pokemon_at():
     count = 0
 
     for pokemon in pokemones:
+        # buscar los poquemones que tienen la cadena 'at'
         if pokemon.find(cadena_buscar) > 0:
             lst_aux = []
-            for pos,char in enumerate(pokemon):
+            # contar la cantidad de a que hay en el nombre del pokemon
+            for pos, char in enumerate(pokemon):
                 if char == char_buscar:
                     lst_aux.append(pos)
             if len(lst_aux) == 2:
                 count += 1
-
 
     return count
 
 
 # funcion para conocer con cuantas especies de pokemon puede procrear raichu. Retorna un numero
 def raichu_egg():
-    pass
+    especie_a_buscar = "raichu"
+    pokemon_egg_group = []
+    aux = []
+    # obtener los egg_group
+    egg_groups = get_egg_group_especie(especie_a_buscar)
+    # junto todos en una lista
+    for egg_group in egg_groups:
+        aux.extend(get_pokemon_por_egg_group(egg_group))
+    print(len(aux))
+
+    # elimino los duplicados
+    pokemon_egg_group = list(set(aux))
+
+    return pokemon_egg_group
 
 
 # funcion para conocer el peso maximo y minimo de los pokemon de tipo fighting de primera generacion.
@@ -32,4 +46,5 @@ def raichu_egg():
 def cotas_peso_fighting():
     pass
 
-print(get_pokemon_at())
+
+print(raichu_egg())
